5373dcdb51a952da16681d6a33ffa8c8f3f577c9
andre <andre@DESKTOP-1RBFGM1>
2022-05-18 14:17:50 -0500

    Add project files.

---
+++ code-challenge/Models/Compensation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+    public class Compensation
+    {
+        [Key]
+        public String EmployeeId { get; set; }
+        public int Salary { get; set; }
+        public string EffectiveDate { get; set; }
+    }
+}
---
+++ code-challenge/Services/EmployeeService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        public Employee Create(Employee employee)
+        {
+            if(employee != null)
+            {
+                _employeeRepository.Add(employee);
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return employee;
+        }
+
+        public Employee GetById(string id)
+        {
+            if(!String.IsNullOrEmpty(id))
+            {
+                return _employeeRepository.GetById(id);
+            }
+
+            return null;
+        }
+
+        public Employee Replace(Employee originalEmployee, Employee newEmployee)
+        {
+            if(originalEmployee != null)
+            {
+                _employeeRepository.Remove(originalEmployee);
+                if (newEmployee != null)
+                {
+                    // ensure the original has been removed, otherwise EF will complain another entity w/ same id already exists
+                    _employeeRepository.SaveAsync().Wait();
+
+                    _employeeRepository.Add(newEmployee);
+                    // overwrite the new id with previous employee id
+                    newEmployee.EmployeeId = originalEmployee.EmployeeId;
+                }
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return newEmployee;
+        }
+
+        public ReportingStructure CountDirectReports(string id)
+        {
+            //Newing up a ReportingStructure instance, you could also use DI service for this
+            ReportingStructure employeeToCount = new ReportingStructure();
+            //code to count the direct reports
+            int totalReports = 0;
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                //Adding the employee object to the ReportingStructure object
+                employeeToCount.Employee = _employeeRepository.GetById(id);
+
+
+                //Counting the direct reports of our direct reports
+                foreach (Employee report in employeeToCount.Employee.DirectReports ?? Enumerable.Empty<object>())
+                {
+                    //Counting each direct report
+                    totalReports++;
+
+                    foreach(var reportOfReport in report.DirectReports ?? Enumerable.Empty<object>())
+                    {
+                        //Counting each report of the direct report
+                        totalReports++;
+                    }
+                    
+                }
+                
+                //Adding the total direct report amount to our ReportingStructure object
+                employeeToCount.NumberOfReports = totalReports;
+            }
+
+            //Return the ReportingStructure (employee record and number of reports)
+            return employeeToCount;
+                
+        }
+    }
+}
